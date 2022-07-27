6a0e5ffe58363fae38f5e154d858cac029946b7a
Ramkrishn <ram706860@gmail.com>
2022-07-27 12:36:30 +0530

    firstcommit

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ var theme = document.getElementsByTagName('link')[1];
 const btnBody = document.getElementsByClassName('.btns');
 const btns = document.getElementsByTagName('span');
 
-for (let i = 4;i< 7; i++){
+for (let i = 4; i < 7; i++) {
     btns[i].addEventListener('click', function () {
-        if (i ==4 ) {
+        if (i == 4) {
             theme.setAttribute('href', 'theme1.css');
-        } else if (i==5) {
-            theme.setAttribute('href', 'theme2.css');            
+        } else if (i == 5) {
+            theme.setAttribute('href', 'theme2.css');
         } else {
-            theme.setAttribute('href', 'theme3.css');            
+            theme.setAttribute('href', 'theme3.css');
         }
     });
 }
@@ -19,25 +19,48 @@ for (let i = 4;i< 7; i++){
 // calculator logic
 const consoleInput = document.getElementById('main-input');
 const numBtns = document.querySelectorAll('.grid-items');
-// console.log(consoleInput);
+
 var result = '';
-function consoleOutPut(result){
+function consoleOutPut(result) {
     consoleInput.setAttribute('value', result);
 }
 
 
-for (let j = 0; j < numBtns.length; j++){
+for (let j = 0; j < numBtns.length; j++) {
     let btnValue = numBtns[j].innerText;
-    // console.log(btnValue)
-    numBtns[j].addEventListener('click', function() {
-        if (btnValue == 1 || btnValue == 2 || btnValue == 3 || btnValue == 4 ||
-            btnValue == 5 || btnValue == 6 || btnValue == 7 || btnValue == 8 || btnValue == 9 || btnValue == 0) {
-                
-                result += btnValue;
+
+    numBtns[j].addEventListener('click', function () {
+        result += btnValue;
+
+        // evalution of the entered values
+        
+        if (btnValue == '=' || btnValue == 'DEL' || btnValue == 'RESET') {
+            result = result.slice(0, result.length - 1)
+
+            if (btnValue == 'DEL') {
+                result = result.slice(0, result.length - 3);
+                consoleOutPut(result);
+            } else if (btnValue == 'RESET') {
+                result = result.slice(0, result.length - 5);
+                result = '';
+                consoleOutPut(result);
+            } else {
+                result = eval(result);
+                consoleOutPut(result);
+            }
+        }
+
+        // Taking Number input 
+        else {
             consoleOutPut(result);
+            if (btnValue == '+' || btnValue == '-' || btnValue == '*' || btnValue == '/') {
+                result += '';
+            }
         }
 
     });
+
+
 }
 
 
